aab7083977ff4f3ac320075256868d5244f1237e
NIKHIL <160263437+0NIKHIL4@users.noreply.github.com>
2026-04-03 17:18:14 +0530

    Fix base path and optimize dependencies in config

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,11 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
+// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: "/AAJ_SUPPLY_ASSINGMENT/"   // 👈 EXACT repo name
+  optimizeDeps: {
+    exclude: ['lucide-react'],
+  },
+  base: "/AAJ_SUPPLY_ASSINGMENT/",   // 👈 GitHub repo name
 })
