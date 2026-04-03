685f3e6a00907ca06e754e3de7b7b0976f4b026d
NIKHIL <160263437+0NIKHIL4@users.noreply.github.com>
2026-04-03 17:10:34 +0530

    Fix typo in repo name in vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -3,5 +3,5 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
-  base: "/AAJ_SUPPLY_ASSIGNMENT/"   // 👈 your repo name
+  base: "/AAJ_SUPPLY_ASSINGMENT/"   // 👈 EXACT repo name
 })
